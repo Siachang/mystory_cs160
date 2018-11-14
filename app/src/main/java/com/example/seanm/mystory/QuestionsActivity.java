a6d500718d46b3bcb20c70f71227aa142e90b016
Emma  Nguyen <emma@Emmas-MacBook-Pro-2.local>
2018-11-13 22:49:33 -0800

    added recycler for questions

---
+++ app/src/main/java/com/example/seanm/mystory/QuestionsActivity.java
@@ -0,0 +1,56 @@
+package com.example.seanm.mystory;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+
+public class QuestionsActivity extends AppCompatActivity {
+
+
+
+
+    private ArrayList<questionCard> questions;
+    private questionCardAdapter adapter;
+    Context context;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        questions = new ArrayList<>();
+        context = this;
+
+
+        //RETRIEVE QUESTIONS
+
+
+        initRecyclerView(questions);
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_questions);
+    }
+
+
+
+    private void initRecyclerView(ArrayList<questionCard> questions) {
+
+        //getting the recyclerview from xml
+        RecyclerView recyclerView = findViewById(R.id.recyclerView);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+
+        //creating recyclerview adapter
+        adapter = new questionCardAdapter(this, questions);
+
+        //setting adapter to recyclerview
+        recyclerView.setAdapter(adapter);
+
+    }
+
+}
